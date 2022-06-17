2f3b47309c672a43b55368cadce7367526d8af18
Smriti Garg <smriti.garg@meredith.com>
2022-06-17 16:08:38 +0530

    stop propagation in bubbling

--- concepts/index.js
+++ concepts/index.js
@@ -1,15 +1,19 @@
-document.querySelector("#grandparent").addEventListener('click', ()=>{
+document.querySelector("#grandparent").addEventListener('click', (e)=>{
     console.log("GRANDPARENT CLICKED");
-}, true); // capturing
+
+}, false); 
 
 
-document.querySelector('#parent').addEventListener('click', () =>{
-    console.log('PARENT CLICKED')
-}, true);// capturing
+document.querySelector('#parent').addEventListener('click', (e) =>{
+    console.log('PARENT CLICKED');
+}, false);
 
-document.querySelector('#child').addEventListener('click', () =>{
-    console.log('CHILD CLICKED')
-}, true)// capturing
+document.querySelector('#child').addEventListener('click', (e) =>{
+    console.log('CHILD CLICKED');
+    e.stopPropagation()
+}, false);
 
 // change true to false and it will convert to bubbling.
 // in event cycle first caputring happens then bubbling by default.
+
+// stopPropagation => bubbling and capturing are quite expensive functionality so this function stops the propapgation of event further
