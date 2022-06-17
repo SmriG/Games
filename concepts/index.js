2dc8a539ee773b2008154921ea0177e1ffd0ecb0
Smriti Garg <smriti.garg@meredith.com>
2022-06-17 15:52:14 +0530

    event delegation -> capturing

--- concepts/index.js
+++ concepts/index.js
@@ -1,15 +1,15 @@
 document.querySelector("#grandparent").addEventListener('click', ()=>{
     console.log("GRANDPARENT CLICKED");
-}, false); // bubbling
+}, true); // capturing
 
 
 document.querySelector('#parent').addEventListener('click', () =>{
     console.log('PARENT CLICKED')
-}, false);// bubbling
+}, true);// capturing
 
 document.querySelector('#child').addEventListener('click', () =>{
     console.log('CHILD CLICKED')
-}, false)// bubbling
+}, true)// capturing
 
 // change true to false and it will convert to bubbling.
 // in event cycle first caputring happens then bubbling by default.
