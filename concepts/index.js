01c3dfeb95b135fa2f6ff9de679c9c75fb8b1fdf
Smriti Garg <smriti.garg@meredith.com>
2022-06-17 16:43:57 +0530

    event bubbling example

--- concepts/index.js
+++ concepts/index.js
@@ -1,13 +1,14 @@
 document.querySelector("#grandparent").addEventListener('click', (e)=>{
     console.log("GRANDPARENT CLICKED");
-    e.stopPropagation()
 
-}, true); 
+}, false); 
 
 
 document.querySelector('#parent').addEventListener('click', (e) =>{
     console.log('PARENT CLICKED');
-}, true);
+    e.stopPropagation()
+
+}, false);
 
 document.querySelector('#child').addEventListener('click', (e) =>{
     console.log('CHILD CLICKED');
@@ -17,3 +18,14 @@ document.querySelector('#child').addEventListener('click', (e) =>{
 // in event cycle first caputring happens then bubbling by default.
 
 // stopPropagation => bubbling and capturing are quite expensive functionality so this function stops the propapgation of event further
+
+// stopPropagation will prevent any parent handlers from being executed stopImmediatePropagation will prevent any parent handlers and also any other handlers from executing
+
+const list = document.querySelector("#name_list ul");
+list.addEventListener('click', (e) => {
+    if(e.target.className=="name"){
+    const item = e.target;
+    list.removeChild(item);
+
+    }
+})
