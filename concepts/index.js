8842105570fb230def560e56e0e6abdd4fb47fa1
Smriti Garg <smriti.garg@meredith.com>
2022-07-14 17:07:00 +0530

    Basic promise

--- concepts/index.js
+++ concepts/index.js
@@ -1,31 +1,16 @@
-document.querySelector("#grandparent").addEventListener('click', (e)=>{
-    console.log("GRANDPARENT CLICKED");
+// Promise is a class which accepts a function which takes two arguments: resolve and reject.
 
-}, false); 
-
-
-document.querySelector('#parent').addEventListener('click', (e) =>{
-    console.log('PARENT CLICKED');
-    e.stopPropagation()
-
-}, false);
-
-document.querySelector('#child').addEventListener('click', (e) =>{
-    console.log('CHILD CLICKED');
-}, true);
-
-// change true to false and it will convert to bubbling.
-// in event cycle first caputring happens then bubbling by default.
-
-// stopPropagation => bubbling and capturing are quite expensive functionality so this function stops the propapgation of event further
-
-// stopPropagation will prevent any parent handlers from being executed stopImmediatePropagation will prevent any parent handlers and also any other handlers from executing
-
-const list = document.querySelector("#name_list ul");
-list.addEventListener('click', (e) => {
-    if(e.target.className=="name"){
-    const item = e.target;
-    list.removeChild(item);
-
+let myPromise = new Promise(function(resolve, reject){
+    const done = false;
+    if(done){
+        let what = "This promise is successful";
+        resolve(what);
+    }else{
+        let why = "This promise is rejected";
+        reject(why);
     }
-})
+
+});
+
+myPromise.then(result=>console.log(result))
+.catch(err=>console.log(err))
