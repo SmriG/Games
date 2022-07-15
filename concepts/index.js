58229ec8b1f644439178a5080632ed56b11a1049
Smriti Garg <smriti.garg@meredith.com>
2022-07-15 17:26:54 +0530

    imporving then implementation

--- concepts/index.js
+++ concepts/index.js
@@ -1,34 +1,61 @@
 const STATE = {
-    FULFILLED: "fulfilled",
-    REJECTED: 'rejected',
-    PENDING: 'pending',
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+  PENDING: "pending",
 };
 
 class MyPromise {
-    #thenCbs = []; // There can be many .then method on a prmoise.
-    #state;
-    #value;
-    constructor(cb){
-        try {
-            cs(this.#onSuccess, this.#onFail)
-        } catch (error) {
-            this.onFail(error)
-        }
+  #thenCbs = []; // There can be many .then method on a prmoise.
+  #catchCbs = [];
+  #state;
+  #value;
+  constructor(cb) {
+    try {
+      cs(this.#onSuccess, this.#onFail);
+    } catch (error) {
+      this.onFail(error);
     }
-    #onSuccess(value){ //resolve method
-        this.#value = value;
-        this.#state = STATE.FULFILLED;
+  }
+  #runCallbacks() {
+    if (this.#state === STATE.FULFILLED) {
+      this.#thenCbs.forEach((callback) => {
+        callback(this.#value);
+      });
+      this.#thenCbs = [];
+    }
+    if (this.#state === STATE.REJECTED) {
+      this.#catchCbs.forEach((callback) => {
+        callback(this.#value);
+      });
+      this.#catchCbs = [];
+    }
+  }
+  #onSuccess(value) {
+    //resolve method
+    if (this.#state !== STATE.PENDING) return;
+    this.#value = value;
+    this.#state = STATE.FULFILLED;
+    this.#runCallbacks();
+  }
+  #onFail(value) {
+    //reject method
+    if (this.#state !== STATE.PENDING) return;
+    this.#value = value;
+    this.#state = STATE.REJECTED;
+    this.#runCallbacks();
+  }
+  then(thenCb, catchCb) { // then has to handle both the fulfilled and rejected state because on then we can pass two handlers.
+    if (thenCb != null) this.#thenCbs.push(thenCb);
+    if (catchCb != null) this.#catchCbs.push(catchCb);
 
-    }
-    #onFail(value){ //reject method
-
-    }
-    then(cb){
-        this.#thenCbs.push(cb)
-    }
-
+    this.#runCallbacks();
+  }
+  catch(cb) {
+    this.then(undefined, cb);
+  }
+  finally(cb) {}
 }
 
 export default MyPromise;
 
-new Promise(cb)
+new Promise(cb);
