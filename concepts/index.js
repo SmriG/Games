8948c50f02f8b64a2efbc43df394887771dcba04
Smriti Garg <smriti.garg@meredith.com>
2022-07-18 15:58:21 +0530

    Start digital clock

--- concepts/index.js
+++ concepts/index.js
@@ -3,15 +3,17 @@ const STATE = {
   REJECTED: "rejected",
   PENDING: "pending",
 };
-
+// CHAINING
 class MyPromise {
   #thenCbs = []; // There can be many .then method on a prmoise.
   #catchCbs = [];
   #state;
   #value;
+  #onSuccessBind = this.#onSuccess.bind(this);
+  #onFailBind = this.#onFail.bind(this);
   constructor(cb) {
     try {
-      cs(this.#onSuccess, this.#onFail);
+      cb(this.#onSuccessBind, this.#onFailBind);
     } catch (error) {
       this.onFail(error);
     }
@@ -33,22 +35,51 @@ class MyPromise {
   #onSuccess(value) {
     //resolve method
     if (this.#state !== STATE.PENDING) return;
+    if (value instanceof MyPromise) {
+      value.then(this.#onSuccessBind, this.#onFailBind);
+      return;
+    }
     this.#value = value;
     this.#state = STATE.FULFILLED;
     this.#runCallbacks();
   }
   #onFail(value) {
     //reject method
-    if (this.#state !== STATE.PENDING) return;
-    this.#value = value;
-    this.#state = STATE.REJECTED;
-    this.#runCallbacks();
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
+      this.#value = value;
+      this.#state = STATE.REJECTED;
+      this.#runCallbacks();
+    });
   }
-  then(thenCb, catchCb) { // then has to handle both the fulfilled and rejected state because on then we can pass two handlers.
-    if (thenCb != null) this.#thenCbs.push(thenCb);
-    if (catchCb != null) this.#catchCbs.push(catchCb);
+  then(thenCb, catchCb) {
+    // then has to handle both the fulfilled and rejected state because on then we can pass two handlers.
+    return new MyPromise((resolve, reject) => {
+      this.#thenCbs.push((result) => {
+        if (thenCb == null) {
+          resolve(result);
+          return;
+        }
+        try {
+          resolve(thenCb(result));
+        } catch (error) {
+          reject(error);
+        }
+      });
+      this.#catchCbs.push((result) => {
+        if (catchCb == null) {
+          reject(result);
+          return;
+        }
+        try {
+          resolve(catchCb(result));
+        } catch (error) {
+          reject(error);
+        }
+      });
 
-    this.#runCallbacks();
+      this.#runCallbacks();
+    });
   }
   catch(cb) {
     this.then(undefined, cb);
