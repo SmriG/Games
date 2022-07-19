c4d6d998cbb71dac7912e4abcd6be95e8a05844f
Smriti Garg <smriti.garg@meredith.com>
2022-07-19 16:32:40 +0530

    adding alarm

--- clock/index.js
+++ clock/index.js
@@ -1,11 +1,11 @@
 let weekdays = {
-  1: "MONDAY",
-  2: "TUESDAY",
-  3: "WEDNESDAY",
-  4: "THURSDAY",
-  5: "FRIDAY",
-  6: "SATURDAY",
-  7: "SUNDAY",
+  1: "MON",
+  2: "TUES",
+  3: "WED",
+  4: "THURS",
+  5: "FRI",
+  6: "SAT",
+  7: "SUN",
 };
 
 function getTime() {
@@ -17,15 +17,17 @@ function getTime() {
   let minute = date.getMinutes();
   let second = date.getSeconds();
   let day = date.getDay();
+  let session = changeSession(hour);
 
   month = addZero(month);
   minute = addZero(minute);
   second = addZero(second);
   day = changeToDay(day);
-  todayDate = addZero(todayDate)
-  const el = document.querySelector(".time");
-  const time = hour + ":" + minute + ":" + second
-  el.innerText = time;
+  todayDate = addZero(todayDate);
+
+  const elTime = document.querySelector(".time");
+  const time = day+ " : " + Math.abs(hour - 12) + " : " + minute + " : " + second + " " + session;
+    elTime.innerHTML = time;
 }
 
 function addZero(value) {
@@ -39,6 +41,14 @@ function changeToDay(value) {
   return weekdays[value];
 }
 
-window.onload = function(){
-  setInterval(getTime, 500)
+function changeSession(hour) {
+  if (hour > 12) {
+    return "PM";
+  } else {
+    return "AM";
+  }
 }
+
+window.onload = function () {
+  setInterval(getTime, 500);
+};
